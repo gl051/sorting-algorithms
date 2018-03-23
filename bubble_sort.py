63152e1a6acc8ef46b2f5105aa29b0b932c4dce2
Gianluca <gl051@biccari.com>
2018-03-23 08:54:23 -0700

    Use range instead of xrange

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,6 @@
 
 """
     Bubble Sort Algorithm
-    Author: Gianluca Biccari
     Description: sort an input array of integers and return a sorted array
 """
 
@@ -11,7 +10,7 @@ def bubble_sort(iarray):
     n = len(iarray)
     while switched:
         switched = False
-        for i in xrange(0, n-1):
+        for i in range(0, n-1):
             if iarray[i] > iarray[i+1]:
                 iarray[i], iarray[i+1] = iarray[i+1], iarray[i]
                 switched = True
