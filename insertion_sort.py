407ba6de630aa102fc807a9040cc3eb494bc10b1
Gianluca <gl051@biccari.com>
2018-03-27 08:28:23 -0700

    Python 3 adjustment

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,7 +2,6 @@
 
 """
     Insertion Sort Algorithm
-    Author: Gianluca Biccari
     Description: sort an input array of integers and return a sorted array
 """
 
@@ -13,10 +12,11 @@ def insertion_sort(iarray):
         N = len(iarray)
         # first element of array already in order, at pos 0
         # start scanning the following elements
-        for j in xrange(1, N):
+        for j in range(1, N):
+            print("{}|{}".format(iarray[:j], iarray[j]))
             # for each scanned element position it inside the
             # already sorted array going from 0 to J-1
-            for i in xrange(0, j):
+            for i in range(0, j):
                 # place element in the right position
                 if iarray[j] <= iarray[i]:
                     tmp = iarray[j]
